3ce18d12f386b23d0053e66508ff381233c903fa
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-12-14 20:27:27 +0530

    Create SuppressWarnings.java

---
+++ Annotations/SuppressWarnings.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String a[]){
+        @SuppressWarnings("")
+        ArrayList list = new ArrayList(); // Here i can't specify the GENERIC <>, that warning ignore
+        System.out.println("Hello");
+    }
+} 
